918bef1da64617c8bc3ad4b7292d32d33c3bf3cc
ALXP-DANIEL <alifdaniel.personalspace@gmail.com>
2026-05-14 14:04:14 +0800

    feat: integrate AppHeader component into new pickup page and update layout

--- frontend/src/app/pages/customer/new-pickup/new-pickup.component.ts
+++ frontend/src/app/pages/customer/new-pickup/new-pickup.component.ts
@@ -1,8 +1,12 @@
+import { AppHeaderComponent } from '@/components/header/header.component';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-customer-new-pickup-page',
   templateUrl: './new-pickup.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [
+    AppHeaderComponent
+  ],
 })
 export class CustomerNewPickupPage {}
